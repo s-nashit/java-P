32c7178db3b56f9c05c1231a4195102415e7538c
Nashit Humam <s_nashit@hotmail.com>
2024-05-17 10:49:43 +0530

    initial practice java

---
+++ Nashit.java
@@ -0,0 +1,15 @@
+
+class Nashit {
+    public static void main(String args[]) {
+        // System.out.println("Nashit");
+        // System.out.println("Nashit");
+        // System.out.print(6 + 7);
+        // float a = 6;
+
+        // byte a = 128;
+        // float a = 6.2f;
+        boolean a = true;
+
+        System.out.print(a);
+    }
+}
---
+++ Shrey.java
@@ -0,0 +1,110 @@
+
+class Shrey {
+    public static void main(String args[]) {
+
+        // int a = 0;
+
+        for (int a = 0; a <= 10; a++) {
+            System.out.println("Hello world");
+        }
+
+        // do {
+        // System.out.println(a);
+        // a += 5;
+        // } while (a <= 100);
+
+        // while (a <= 10) {
+        // System.out.println("Thank you");
+        // a++;
+        // }
+
+        // int grade = 8;
+
+        // switch (grade) {
+        // case 1:
+        // System.out.print("Grade 10");
+        // break;
+        // case 2:
+        // System.out.print("Grade 9");
+        // break;
+        // case 3:
+        // System.out.print("Grade 8");
+        // break;
+        // case 4:
+        // System.out.print("Grade 7");
+        // break;
+        // case 5:
+        // System.out.print("Grade 6");
+        // break;
+        // case 6:
+        // System.out.print("Grade 5");
+        // break;
+        // case 7:
+        // System.out.print("Grade 4");
+        // break;
+        // case 8:
+        // System.out.print("Grade 3");
+        // break;
+        // case 9:
+        // System.out.print("Grade 2");
+        // break;
+        // case 10:
+        // System.out.print("Grade A");
+        // break;
+
+        // }
+
+        // if (marks >= 75) {
+        // System.out.print("Distinction");
+        // } else if (marks >= 60 && marks < 75) {
+        // System.out.print("First Class");
+        // } else if (marks >= 50 && marks < 60) {
+        // System.out.print("Second Class");
+        // } else if (marks >= 40 && marks < 50) {
+        // System.out.print("Pass");
+        // } else if (marks < 40) {
+        // System.out.print("Fail");
+        // } else {
+        // System.out.print("Invalid marks");
+        // }
+
+        // System.out.print((marks >= 40) ? "Pass" : "Fail");
+
+        // condition ? true : false
+
+        // if (marks >= 40) {
+        // System.out.print("Pass");
+        // } else {
+        // System.out.print("Fail");
+        // }
+        // int age = 10;
+
+        // if (age >= 18) {
+        // System.out.print("You can vote");
+        // } else {
+        // System.out.print("You cannot vote");
+        // }
+        // System.out.print(age == 19);
+        // String a = "Nashit Humam";
+
+        // System.out.print(a.lastIndexOf('a'));
+        // System.out.print(a.codePointBefore(6));
+        // String a = "s_nashit@hotmail@yahoo@gmail@com";
+        // System.out.print(a.split("@")[2]);
+
+        // String b = "ranjit";
+        // int c = 54;
+        // System.out.print(a);
+
+        // System.out.print(a.trim());
+        // System.out.print(a.toString());
+
+        // System.out.print(a.replaceFirst("a", "A"));
+        // System.out.print(a.startsWith("n"));
+        // System.out.print(a.replace('s', 'W'));
+        // System.out.print(String.join(" weds ", "Thadki", "Sanju"));
+        // System.out.print(a.compareTo(b));
+        // System.out.print("damodar".concat(" is from Patna"));
+
+    };
+}
